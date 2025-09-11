304d7714d079961248b7123198abd23d581220cb
jaehyeong <sundae0609@gmail.com>
2025-09-11 16:23:12 +0900

    양계장 프로젝트 1차 백엔드 생성

---
+++ src/main/java/com/backend/chickenFarm/ChickenFarmApplication.java
@@ -0,0 +1,13 @@
+package com.backend.chickenFarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChickenFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChickenFarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/chickenFarm/ChickenFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.chickenFarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChickenFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
