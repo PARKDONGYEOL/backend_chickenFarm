181a38d65eef11917f4817dcca0fcb3c51f9c895
Taeee372 <parktk99@gmail.com>
2025-10-10 17:34:32 +0900

    폐사 처리, 건강 상태 수정

--- src/main/java/com/backend/chickenFarm/chicken_farm/controller/ChickenFarmController.java
+++ src/main/java/com/backend/chickenFarm/chicken_farm/controller/ChickenFarmController.java
@@ -5,10 +5,9 @@ import com.backend.chickenFarm.chicken_farm.service.ChickenFarmService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,6 +26,20 @@ public class ChickenFarmController {
       return ResponseEntity.status(HttpStatus
               .INTERNAL_SERVER_ERROR)
               .body("양계장 등록 쿼리 실행 중 오류가 발생했습니다.");
+    }
+  }
+
+  //양계장 번호 조회
+  @GetMapping("/num-list")
+  public ResponseEntity<?> getFarmInfo(){
+    try {
+      List<ChickenFarmDTO> numList = chickenFarmService.getFarmInfo();
+      return ResponseEntity.status(HttpStatus.OK).body(numList);
+    }catch (Exception e){
+      e.printStackTrace();
+      return ResponseEntity
+              .status(HttpStatus.INTERNAL_SERVER_ERROR)
+              .body("양계장 번호 조회 쿼리 실행 중 오류가 발생했습니다.");
     }
   }
 }
