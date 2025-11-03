5a7c6a3e899d8ee96afcfcf291c332a42f3aced9
Taeee372 <parktk99@gmail.com>
2025-11-03 15:03:12 +0900

    관리자 변경

--- src/main/java/com/backend/chickenFarm/CorsConfig.java
+++ src/main/java/com/backend/chickenFarm/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://192.168.30.95:5173", "http://192.168.30.152:5173")
+            .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://192.168.30.95:5173", "http://192.168.30.111:5173")
             //.allowedOrigins("**")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
             .allowedHeaders("*")
